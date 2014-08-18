2e717eb2802816f36347de1345edf525a76a52df
arturomc <4rturo.m.c@gmail.com>
2014-08-18 18:07:01 -0500

    added backend API stuff

--- server/APIs/ContactsApi.js
+++ server/APIs/ContactsApi.js
@@ -9,12 +9,30 @@ module.exports = function(server) {
         next();
     });
 
+    //get a list of all the contacts in the DB
     router.get('/contacts', function(req, res){
         console.log('get a contact');
+        Contact.find(function(err, contacts) {
+            if (err) {
+                res.send("Something got screwed up")
+            }
+            res.json(contacts);
+        })
     });
 
+    router.get('/contact/:contact_id', function(req,res){
+        console.log('getting a contact by id');
+        Contact.findById(req.params.contact_id, function(err, contact){
+            if (err) {
+                res.send('this crashes all the time...')
+            }
+            res.json(contact);
+        });
+    });
+
+    //insert a Contact record to the DB
     router.post('/contacts', function(req, res){
-        console.log(req);
+        console.log('inserting a contact ...');
         var contact = new Contact;
         var rb = req.body;
 
@@ -31,8 +49,46 @@ module.exports = function(server) {
         });
 
         res.json(
-            rb
+            {success:contact}
         )
+    });
+
+    //remove a contact record by _id
+    router.delete('/contact/:contact_id', function(req,res){
+        console.log('deleting a contact ...');
+
+        Contact.remove({
+            _id:req.params.contact_id
+        }, function(err, contact){
+            if(err) {
+                console.log("Something went really, really bad! You better call 911")
+            }
+            res.json({
+                message:'Record deleted successfully!'
+            })
+        })
+    });
+
+    router.put('/contact/:contact_id', function(req, res) {
+        Contact.findById(request.params.contact_id, function(err, contact) {
+            console.log(contact);
+            if(err) {
+                res.send(err);
+            }
+            contact.fname   = rb.fname;
+            contact.lname   = rb.lname;
+            contact.where   = rb.where;
+            contact.phone   = rb.phone;
+            contact.address = rb.address;
+            contact.location= rb.location;
+            contact.info    = rb.info;
+            contact.save(function(err){
+                if(err) {
+                    response.send (err);
+                }
+            });
+            response.json(request.body)
+        });
     });
 
     router.get('/hc-contacts', function(req, res) {
@@ -59,6 +115,8 @@ module.exports = function(server) {
             ]
         )
     });
+
+
 
     server.use('/api', router);
 };
